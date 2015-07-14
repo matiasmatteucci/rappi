8edda864d8ef4a847902bf66701ddf719be720be
Matias Matteucci <matias.matteucci@jivesoftware.com>
2015-07-14 18:25:59 -0300

    Uploading source code to git.

---
+++ src/test/java/constants/gmailTestConstants.java
@@ -0,0 +1,27 @@
+package constants;
+
+/**
+ * Created by Matias on 12/06/2015.
+ */
+public class gmailTestConstants {
+    public static String URL = "https://mail.google.com";
+    public static String USERNAME = "test.globallogic1@gmail.com";
+    public static String PASSWORD = "guessme123";
+
+    public static String USERNAME_FIELD = "input#Email";
+    public static String NEXT_BUTTON = "input#next";
+    public static String PASSWORD_FIELD = "input#Passwd";
+    public static String SIGN_IN_BUTTON = "input#signIn";
+
+    public static String COMPOSE_BUTTON = "div.T-I.J-J5-Ji.T-I-KE.L3";
+    public static String TO_FIELD = "textarea[name=to]";
+    public static String SUBJECT_FIELD = "input[name=subjectbox]";
+    public static String BODY = "div.Am.Al.editable.LW-avf";
+    public static String SEND_BUTTON = "div.T-I.J-J5-Ji.aoO.T-I-atl.L3";
+
+    public static String TEST_EMAIL = "div.yW";
+    public static String CONTEXT_MENU_DELETE = "div.J-N-JX.aDE.aDF";
+
+    public static String RECEIVED = "div.aio.UKr6le";
+
+}
---
+++ src/test/java/test/gmailTest.java
@@ -0,0 +1,88 @@
+package test;
+
+import constants.gmailTestConstants;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Matias on 12/06/2015.
+ */
+public class gmailTest extends chromeBaseTest {
+
+    private void Login() throws Exception{
+        // Visit site
+        driver.get(gmailTestConstants.URL);
+
+
+        WebElement usernameField = driver.findElement(By.cssSelector(gmailTestConstants.USERNAME_FIELD));
+        WebElement nextButton = driver.findElement(By.cssSelector(gmailTestConstants.NEXT_BUTTON));
+
+
+        usernameField.sendKeys(gmailTestConstants.USERNAME);
+        nextButton.click();
+
+        Thread.sleep(1000);
+
+        WebElement passwordField = driver.findElement(By.cssSelector(gmailTestConstants.PASSWORD_FIELD));
+        WebElement signInButton = driver.findElement(By.cssSelector(gmailTestConstants.SIGN_IN_BUTTON));
+
+        passwordField.sendKeys(gmailTestConstants.PASSWORD);
+        signInButton.click();
+
+        Thread.sleep(10000);
+    }
+    private void composeAndSendEmail() throws Exception {
+        WebElement composeButton = driver.findElement(By.cssSelector(gmailTestConstants.COMPOSE_BUTTON));
+
+        composeButton.click();
+
+        Thread.sleep(2000);
+
+        WebElement toField = driver.findElement(By.cssSelector(gmailTestConstants.TO_FIELD));
+        WebElement subjectField = driver.findElement(By.cssSelector(gmailTestConstants.SUBJECT_FIELD));
+        WebElement body = driver.findElement(By.cssSelector(gmailTestConstants.BODY));
+        WebElement sendButton = driver.findElement(By.cssSelector(gmailTestConstants.SEND_BUTTON));
+
+        toField.sendKeys(gmailTestConstants.USERNAME);
+        subjectField.sendKeys("Super Cool Subject");
+        body.sendKeys("Body of email");
+        sendButton.click();
+
+
+        Thread.sleep(2000);
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+
+        assertThat("Email not found in page", driver.getPageSource(), containsString("Super Cool Subject"));
+    }
+    private void deleteEmail() throws Exception{
+
+        List<WebElement> emails = driver.findElements(By.cssSelector(gmailTestConstants.TEST_EMAIL));
+
+        Actions action = new Actions(driver);
+        action.contextClick(emails.get(0)).perform();
+
+        WebElement deleteButton = driver.findElement(By.cssSelector(gmailTestConstants.CONTEXT_MENU_DELETE));
+        deleteButton.click();
+
+        Thread.sleep(1000);
+
+    }
+
+
+
+    @Test
+    public void run() throws Exception{
+        Login();
+        composeAndSendEmail();
+        deleteEmail();
+    }
+
+}
